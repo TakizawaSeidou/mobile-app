26b2cb811c14a2afae67b640c8e25fecd4530a9f
user <zumaevanvar36@gmail.com>
2023-06-10 14:58:51 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from './Pages/Head/Head'
+
+function App() {
+  return (
+    <div className="App">
+      <Head/>
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Pages/Head/Head.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from './style.module.css'
+
+
+function Menu(){
+    return(
+     <div></div>
+    )
+}
+export default Menu;
